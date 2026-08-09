5a7b51b1689a60d1242e430cad0727232aab647c
neajamama-max <ne.a.jamama@gmail.com>
2026-08-09 10:55:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ function handleUploadErrors(err, req, res, next) {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use('/uploads', express.static(uploadDir));
 
 // ============================================================
